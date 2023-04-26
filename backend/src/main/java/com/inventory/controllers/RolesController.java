93f028bdd28f3238c4256063574a35b31c8c96e6
Aadhithya-incedo <131429186+Aadhithya-incedo@users.noreply.github.com>
2023-04-26 11:55:18 +0530

    Added 3rd column to the link table

--- backend/src/main/java/com/inventory/controllers/RolesController.java
+++ backend/src/main/java/com/inventory/controllers/RolesController.java
@@ -1,10 +1,14 @@
 package com.inventory.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,23 @@ public class RolesController {
 	public void addUser(@RequestBody Roles r) {
 		rolesRepository.save(r);
 	}
+	
+	@DeleteMapping(path = "/roles/{id}")
+	public void deleteUser(@PathVariable long id) {
+		Optional<Roles> roleFound = rolesRepository.findById(id);
+		if(roleFound.isPresent()) {
+			rolesRepository.delete(roleFound.get());
+		}
+	}
+	
+	@PutMapping(path = "/roles")
+	public void updateUser(@RequestBody Roles r) {
+		Optional<Roles> roleFound = rolesRepository.findById(r.getId());
+		if(roleFound.isPresent()) {
+			r = roleFound.get();
+			
+			rolesRepository.save(r);
+		}
+	}
+	
 }
