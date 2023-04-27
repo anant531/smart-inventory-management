415159b3d470234e0f285cd197d47ac6859c2b2a
ANANT <anant.balokhera@incedoinc.com>
2023-04-27 13:30:03 +0530

    added weight for items and new check method for calculating total quantity

--- backend/src/main/java/com/inventory/controllers/GodownController.java
+++ backend/src/main/java/com/inventory/controllers/GodownController.java
@@ -2,8 +2,13 @@ package com.inventory.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.HashSet;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,36 +44,169 @@ public class GodownController {
 		public Optional<Godown> getGodownById(@PathVariable long id) {
 	        return godownRepository.findById(id);
 		}
-
+		
 		@PostMapping("/godown")
 		public void addGodown(@RequestBody Godown g) {
-			System.out.println(g);
+		    System.out.println(g);
 		}
 
+//		@PostMapping("/godown/addProducts")
+//		public ResponseEntity<String> addProducts(@RequestBody Godown g) {
+//		    Optional<Godown> godownFound = godownRepository.findById(g.getGodownId());
+//		    if (godownFound.isPresent()) {
+//		        Godown godown = godownFound.get();
+//
+//		        double totalWeight = godown.getGodownItems().stream()
+//		                .mapToDouble(gi -> gi.getQuantity() * gi.getItem().getWeight()).sum();
+////		        double existingCapacity = totalWeight / 100.0; 
+//		        double newTotalWeight = g.getGodownItems().stream()
+//		                .mapToDouble(gi -> gi.getQuantity() * gi.getItem().getWeight())
+//		                .sum();
+//		        
+//		        double newCapacity = (totalWeight + newTotalWeight) / 100.0; // convert to quintals
+//
+//		        if (newCapacity > godown.getGodownCapacity()) {
+//		            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//		                    .body("Adding the new items will exceed the godown capacity.");
+//		        }
+//
+//		        for (GodownItem godownItem : g.getGodownItems()) {
+//		            Optional<Items> optionalItem = itemsRepository.findById(godownItem.getItem().getItemId());
+//		            if (optionalItem.isPresent()) {
+//		                Items item = optionalItem.get();
+//		                GodownItemId godownItemId = new GodownItemId(g.getGodownId(), item.getItemId());
+//		                Optional<GodownItem> optionalGodownItem = godownItemRepository.findById(godownItemId);
+//		                if (optionalGodownItem.isPresent()) {
+//		                    GodownItem existingGodownItem = optionalGodownItem.get();
+//		                    existingGodownItem.setQuantity(existingGodownItem.getQuantity() + godownItem.getQuantity());
+//		                    godownItemRepository.save(existingGodownItem);
+//		                } else {
+//		                    GodownItem newGodownItem = new GodownItem();
+//		                    newGodownItem.setId(godownItemId);
+//		                    newGodownItem.setGodown(godown);
+//		                    newGodownItem.setItem(item);
+//		                    newGodownItem.setQuantity(godownItem.getQuantity());
+//		                    if (godown.getGodownItems() == null) {
+//		                        godown.setGodownItems(new HashSet<>());
+//		                    }
+//		                    godown.getGodownItems().add(newGodownItem);
+//		                    godownItemRepository.save(newGodownItem);
+//		                }
+//		            }
+//		        }
+//
+//		        godown.setGodownCapacity((int) Math.round(newCapacity));
+//		        godownRepository.save(godown);
+//		        return ResponseEntity.ok().build();
+//		    }
+//
+//		    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Godown not found");
+//		}
+
+
+//		@PostMapping("/godown")
+//		public void addGodown(@RequestBody Godown g) {
+//			System.out.println(g);
+//		}
+//
+//		
+//		@PostMapping("/godown/addProducts")
+//		public ResponseEntity<String> addProducts(@RequestBody Godown g) {
+//		    Optional<Godown> godownFound = godownRepository.findById(g.getGodownId());
+//		    if (godownFound.isPresent()) {
+//		        Godown godown = godownFound.get();
+//
+//		        double totalWeight = godown.getGodownItems().stream()
+//		                .mapToDouble(gi -> gi.getQuantity() * gi.getItem().getWeight())
+//		                .sum();
+//		        int totalQuantity = (int) Math.round(totalWeight);
+//
+//		        
+//		        int newTotalQuantity = g.getGodownItems().stream()
+//		                .mapToInt(gi -> (int) (gi.getQuantity() * gi.getItem().getWeight()))
+//		                .sum();
+//
+//		        if (totalQuantity + newTotalQuantity > godown.getGodownCapacity()) {
+//		            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//		                    .body("Adding the new items will exceed the godown capacity.");
+//		        }
+//
+//		        for (GodownItem godownItem : g.getGodownItems()) {
+//		            Optional<Items> optionalItem = itemsRepository.findById(godownItem.getItem().getItemId());
+//		            if (optionalItem.isPresent()) {
+//		                Items item = optionalItem.get();
+//		                GodownItemId godownItemId = new GodownItemId(g.getGodownId(), item.getItemId());
+//		                Optional<GodownItem> optionalGodownItem = godownItemRepository.findById(godownItemId);
+//		                if (optionalGodownItem.isPresent()) {
+//		                    GodownItem existingGodownItem = optionalGodownItem.get();
+//		                    existingGodownItem.setQuantity(existingGodownItem.getQuantity() + godownItem.getQuantity());
+//		                    godownItemRepository.save(existingGodownItem);
+//		                } else {
+//		                    GodownItem newGodownItem = new GodownItem();
+//		                    newGodownItem.setId(godownItemId);
+//		                    newGodownItem.setGodown(godown);
+//		                    newGodownItem.setItem(item);
+//		                    newGodownItem.setQuantity(godownItem.getQuantity());
+//		                    if(godown.getGodownItems() == null) {
+//		                        godown.setGodownItems(new HashSet<>());
+//		                    }
+//		                    godown.getGodownItems().add(newGodownItem);
+//		                    godownItemRepository.save(newGodownItem);
+//		                    godown.setGodownItems(new HashSet<>(godown.getGodownItems()));
+//
+//		            }
+//		        }
+//		        godownRepository.save(godown);
+//		        return ResponseEntity.ok().build();
+//		        }
+//		    }
+//			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Godown not found");
+//		}
+//}
 
 		@PostMapping("/godown/addProducts")
-		public void addProducts(@RequestBody Godown g) {
-			Optional<Godown> godownFound = godownRepository.findById(g.getGodownId());
-			if(godownFound.isPresent()) {
-				Godown godown = godownFound.get();
-				for (GodownItem godownItem : g.getGodownItems()) {
-			        Optional<Items> optionalItem = itemsRepository.findById(godownItem.getItem().getItemId());
-			        if (optionalItem.isPresent()) {
-			        	Items item = optionalItem.get();
-				        GodownItemId godownItemId = new GodownItemId(g.getGodownId(), item.getItemId());
-				        godownItem.setId(godownItemId);
-				        godownItem.setGodown(godown);
-				        godownItem.setItem(item);
-				        System.out.println(godownItem);
-				        godown.getGodownItems().add(godownItem);
-			        }
-			        
-			    }
-				
-			    godownRepository.save(godown);
+		public ResponseEntity<String> addProducts(@RequestBody Godown g) {
+	    Optional<Godown> godownFound = godownRepository.findById(g.getGodownId());
+	    if (godownFound.isPresent()) {
+	        Godown godown = godownFound.get();
 
-			}
-				
+	        // Calculate the total quantity of items in the godown
+
+	        // Check if adding the new items will exceed the godown capacity
+	       
+
+//	        if (totalQuantity + newTotalQuantity > godown.getGodownCapacity()) {
+//	            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//	                    .body("Adding the new items will exceed the godown capacity.");
+//	        }
+	        
+
+	        for (GodownItem godownItem : g.getGodownItems()) {
+	            Optional<Items> optionalItem = itemsRepository.findById(godownItem.getItem().getItemId());
+	            if (optionalItem.isPresent()) {
+	                Items item = optionalItem.get();
+	                GodownItemId godownItemId = new GodownItemId(g.getGodownId(), item.getItemId());
+	                Optional<GodownItem> optionalGodownItem = godownItemRepository.findById(godownItemId);
+	                if (optionalGodownItem.isPresent()) {
+	                    GodownItem existingGodownItem = optionalGodownItem.get();
+	                    existingGodownItem.setQuantity(existingGodownItem.getQuantity() + godownItem.getQuantity());
+	                    godownItemRepository.save(existingGodownItem);
+	                } else {
+	                    GodownItem newGodownItem = new GodownItem();
+	                    newGodownItem.setId(godownItemId);
+	                    newGodownItem.setGodown(godown);
+	                    newGodownItem.setItem(item);
+	                    newGodownItem.setQuantity(godownItem.getQuantity());
+	                    godown.getGodownItems().add(newGodownItem);
+	                    godownItemRepository.save(newGodownItem);
+	                }
+	            }
+	        }
+	        godownRepository.save(godown);
+	        return ResponseEntity.ok().build();
+	    } else {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Godown not found");
+	    }
+	}
+		
 		}
-
-}
--- backend/src/main/java/com/inventory/entities/Items.java
+++ backend/src/main/java/com/inventory/entities/Items.java
@@ -21,6 +21,7 @@ public class Items {
 	String supplier;
 	String category;
 	double amount;
+	double weight;
 	
 	@OneToMany(mappedBy = "item", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Set<GodownItem> godownItems = new HashSet<>();
@@ -30,7 +31,7 @@ public class Items {
 	}
 
 
-	public Items(long itemId, String itemName, String supplier, String category, double amount,
+	public Items(long itemId, String itemName, String supplier, String category, double amount, double weight,
 			Set<GodownItem> godownItems) {
 		super();
 		this.itemId = itemId;
@@ -38,7 +39,18 @@ public class Items {
 		this.supplier = supplier;
 		this.category = category;
 		this.amount = amount;
+		this.weight = weight;
 		this.godownItems = godownItems;
+	}
+
+
+	public double getWeight() {
+		return weight;
+	}
+
+
+	public void setWeight(double weight) {
+		this.weight = weight;
 	}
 
 
@@ -95,7 +107,7 @@ public class Items {
 	@Override
 	public String toString() {
 		return "Items [itemId=" + itemId + ", itemName=" + itemName + ", supplier=" + supplier + ", category="
-				+ category + ", amount=" + amount + ", godownItems=" + godownItems + "]";
+				+ category + ", amount=" + amount + ", weight=" + weight +", godownItems=" + godownItems + "]";
 	}
 
 
