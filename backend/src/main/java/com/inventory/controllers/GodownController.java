03a21fb0f373a17ad2792b1364b8eeb9099b8a4c
ANANT <anant.balokhera@incedoinc.com>
2023-05-02 10:36:14 +0530

    Update GodownController.java

--- backend/src/main/java/com/inventory/controllers/GodownController.java
+++ backend/src/main/java/com/inventory/controllers/GodownController.java
@@ -3,13 +3,22 @@ package com.inventory.controllers;
 import com.inventory.embeddable.GodownItemId;
 import com.inventory.entities.Godown;
 import com.inventory.entities.GodownItem;
+import com.inventory.entities.Inward;
 import com.inventory.entities.Items;
 import com.inventory.repositories.GodownItemRepository;
 import com.inventory.repositories.GodownRepository;
+import com.inventory.repositories.InwardRepository;
 import com.inventory.repositories.ItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,12 +27,15 @@ public class GodownController {
 
 		@Autowired
 		GodownRepository godownRepository;
-		
+
 		@Autowired
 		ItemsRepository itemsRepository;
-		
+
 		@Autowired
 		GodownItemRepository godownItemRepository;
+
+		@Autowired
+	    InwardRepository inwardRepository;
 
 		@GetMapping("/godown")
 		public List<Godown> getAllGodown() {
@@ -35,8 +47,10 @@ public class GodownController {
 	        return godownRepository.findById(id);
 		}
 
+
 		@PostMapping("/godown")
 		public void addGodown(@RequestBody Godown g) {
+			System.out.println(g);
 			System.out.println(g);
 		}
 
@@ -85,5 +99,112 @@ public class GodownController {
 			}
 				
 		}
+		public double calcualteCapacity(Godown godown){
+			double totalWeight = godown.getGodownItems().stream()
+					.mapToDouble(gi -> gi.getQuantity() * gi.getItem().getWeight()).sum();
+
+			return ((double) Math.round(totalWeight) / 100);
+		}
+
+//		@PostMapping("/godown/addProducts")
+//		public void addProducts(@RequestBody Godown g, @RequestParam("godownId") Long godownId, @RequestParam("quantity") Integer quantity, @RequestParam("NameofSupplier") String supplierName, @RequestParam("dateOfSupply") Date dateOfSupply, @RequestParam("invoice") long invoiceNo, @RequestParam("received") String receivedBy, @RequestParam("receiptNo") long receiptNo, @RequestParam("billCheckedBy") String billCheckedBy) {
+//			Optional<Godown> godownFound = godownRepository.findById(g.getGodownId());
+//			if(godownFound.isPresent()) {
+//				Godown godown = godownFound.get();
+//				for (GodownItem godownItem : g.getGodownItems()) {
+//			        Optional<Items> optionalItem = itemsRepository.findById(godownItem.getItem().getItemId());
+//			        if (optionalItem.isPresent()) {
+//			        	Items item = optionalItem.get();
+//				        GodownItemId godownItemId = new GodownItemId(g.getGodownId(), item.getItemId());
+//				        Optional<GodownItem> godownItemFound = godownItemRepository.findById(godownItemId);
+//				        if(godownItemFound.isPresent()) {
+//				        	GodownItem existingGodownItem = godownItemFound.get();
+//				        	existingGodownItem.setQuantity(godownItem.getQuantity() + existingGodownItem.getQuantity());
+//
+//							if(calcualteCapacity(godown) <= godown.getGodownCapacity()) {
+//								godownItemRepository.save(existingGodownItem);
+//							}
+//				        }
+//				        else {
+//				        	godownItem.setId(godownItemId);
+//					        godownItem.setGodown(godown);
+//					        godownItem.setItem(item);
+//					        godown.getGodownItems().add(godownItem);
+//						}
+//
+//						Inward inward = new Inward();
+//						inward.setGodown(godown);
+//						inward.setItem(item);
+//						inward.setQuantity(quantity);
+//						inward.setNameofSupplier(supplierName);
+//						inward.setDateOfSupply(dateOfSupply);
+//						inward.setInvoice(invoiceNo);
+//						inward.setReceived(receivedBy);
+//						inward.setReceiptNo(receiptNo);
+//						inward.setBillCheckedBy(billCheckedBy);
+//						inwardRepository.save(inward);
+//			        }
+//			    }
+//
+//				if(calcualteCapacity(godown)  <= godown.getGodownCapacity()){
+//					godownRepository.save(godown);
+//				}
+//
+//				else{
+//					System.out.println("EZ");
+//				}
+//			}
+//		}
+
+	@PostMapping("/godown/addProducts")
+	public void addProducts(@RequestBody Godown g, @RequestParam("godownId") Long godownId, @RequestParam("quantity") Integer quantity, @RequestParam("NameofSupplier") String supplierName, @RequestParam("dateOfSupply") Date dateOfSupply, @RequestParam("invoice") long invoiceNo, @RequestParam("received") String receivedBy, @RequestParam("receiptNo") long receiptNo, @RequestParam("billCheckedBy") String billCheckedBy) {
+		Optional<Godown> godownFound = godownRepository.findById(g.getGodownId());
+		List<Items> allItems = new ArrayList<>();
+		if (godownFound.isPresent()) {
+			Godown godown = godownFound.get();
+			for (GodownItem godownItem : g.getGodownItems()) {
+				Optional<Items> optionalItem = itemsRepository.findById(godownItem.getItem().getItemId());
+				allItems.add(optionalItem.get());
+				if (optionalItem.isPresent()) {
+					Items item = optionalItem.get();
+					GodownItemId godownItemId = new GodownItemId(g.getGodownId(), item.getItemId());
+					Optional<GodownItem> godownItemFound = godownItemRepository.findById(godownItemId);
+					if (godownItemFound.isPresent()) {
+						GodownItem existingGodownItem = godownItemFound.get();
+						existingGodownItem.setQuantity(godownItem.getQuantity() + existingGodownItem.getQuantity());
+
+						if (calcualteCapacity(godown) <= godown.getGodownCapacity()) {
+							godownItemRepository.save(existingGodownItem);
+						}
+					} else {
+						godownItem.setId(godownItemId);
+						godownItem.setGodown(godown);
+						godownItem.setItem(item);
+						godown.getGodownItems().add(godownItem);
+					}
+
+					Inward inward = new Inward();
+					inward.setGodown(godown);
+					inward.setItem(allItems);
+					inward.setQuantity(quantity);
+					inward.setNameofSupplier(supplierName);
+					inward.setDateOfSupply(dateOfSupply);
+					inward.setInvoice(invoiceNo);
+					inward.setReceived(receivedBy);
+					inward.setReceiptNo(receiptNo);
+					inward.setBillCheckedBy(billCheckedBy);
+					inwardRepository.save(inward);
+				}
+			}
+
+			if (calcualteCapacity(godown) <= godown.getGodownCapacity()) {
+				godownRepository.save(godown);
+			} else {
+				System.out.println("EZ");
+			}
+		}
+	}
+
+}
 
 }
