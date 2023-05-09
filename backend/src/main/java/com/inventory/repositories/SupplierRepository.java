e5f3ba96362ea33acd039a20d8b7684806244c64
ANANT <anant.balokhera@incedoinc.com>
2023-05-09 12:05:59 +0530

    added supplier

--- backend/src/main/java/com/inventory/repositories/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.repositories;
-
-import com.inventory.entities.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-}
